a5095e459ed05ff73deb8f532e0831557edac2f6
Philippe Garand <philippe.garand@hotmail.com>
2021-08-26 01:14:44 -0400

    PowerShell scripts now working

--- SqlDump.ps1
+++ SqlDump.ps1
@@ -3,22 +3,32 @@ param(
     [System.String]$BackupOutupFolder
 )
 
-# Change to database_prod AND backup location
-$BackupFolderName = "recipesLibraryBackup_$((Get-Date).ToString('yyyy-MM-dd_HH-mm'))"
-$PicturesSource = "./API/recipesPictures/dev/*"
-$DbContainer = "database_dev"
+# Choose Environment
+$IsDev = Read-Host "Is Dev? [y/n]"
+while($IsDev -ne "y")
+{
+    if ($IsDev -eq 'n') {break}
+    $IsDev = Read-Host "Is Dev? [y/n]"
+}
+$Env = if ($IsDev -eq "y") {"dev"} else {"prod"}
 
-# Create new backup instance folder
+# Variables
+$PicturesSource = "./API/recipesPictures/$($Env)/*"
+$DbContainer = "database_$($Env)"
+$BackupFolderName = "recipesLibraryBackup_$((Get-Date).ToString('yyyy-MM-dd_HH-mm'))"
+
+# Create new backup folder
 New-Item -ItemType Directory -Path $BackupOutupFolder\$BackupFolderName | Out-Null
 
-# Create dump file zip
-docker exec $DbContainer sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD"' > $BackupOutupFolder\$BackupFolderName\db.sql
-Compress-Archive -Path $BackupOutupFolder\$BackupFolderName\db.sql -DestinationPath $BackupOutupFolder\$BackupFolderName\db.zip
-Remove-Item $BackupOutupFolder\$BackupFolderName\db.sql
+# Create dump file zip - Must pass by docker volume because of some encoding problem
+docker exec $DbContainer sh -c 'exec mysqldump --all-databases -uroot -p"$MYSQL_ROOT_PASSWORD" -r /backup/db.sql'
+Compress-Archive -Path .\DB\backupTemp\db.sql -DestinationPath $BackupOutupFolder\$BackupFolderName\db.zip
+Remove-Item .\DB\backupTemp\db.sql
 
 # Zip recipes
 Get-ChildItem -Path $PicturesSource | Compress-Archive -DestinationPath $BackupOutupFolder\$BackupFolderName\pictures.zip
 
+# Done
 Write-Host ""
 Write-Host "Backup created at $($BackupOutupFolder)/$($BackupFolderName)"
 Write-Host ""
--- SqlRestore.ps1
+++ SqlRestore.ps1
@@ -3,23 +3,33 @@ param(
     [System.String]$BakupFolder
 )
 
-# Change to database_prod AND backup location
-$PicturesSourceCleanup = "./API/recipesPictures/dev/*"
-$PicturesSourceFolder = "./API/recipesPictures/dev"
-$DbContainer = "database_dev"
+# Choose Environment
+$IsDev = Read-Host "Is Dev? [y/n]"
+while($IsDev -ne "y")
+{
+    if ($IsDev -eq 'n') {break}
+    $IsDev = Read-Host "Is Dev? [y/n]"
+}
+$Env = if ($IsDev -eq "y") {"dev"} else {"prod"}
+
+# Variables
+$PicturesSourceCleanup = "./API/recipesPictures/$($Env)/*"
+$PicturesSourceFolder = "./API/recipesPictures/$($Env)"
+$DbContainer = "database_$($Env)"
 
 # Unzip db
-Expand-Archive $BakupFolder/db.zip $BakupFolder/temp
-While (!(Test-Path $BakupFolder/temp)) { Start-Sleep 2 }
+Expand-Archive $BakupFolder/db.zip ./DB/backupTemp
+While (!(Test-Path ./DB/backupTemp/db.sql)) { Start-Sleep 2 }
 
-# Restore db
-Get-Content $BakupFolder/temp/db.sql | docker exec -i $DbContainer sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD"'
-Remove-Item -recurse $BakupFolder/temp
+# Restore db - Must pass by docker volume because of some encoding problem
+docker exec -i $DbContainer sh -c 'exec mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /backup/db.sql'
+Remove-Item -recurse ./DB/backupTemp/db.sql
 
 # Restore (unzip) pictures
 Remove-Item $PicturesSourceCleanup
 Expand-Archive -LiteralPath $BakupFolder/pictures.zip -DestinationPath $PicturesSourceFolder
 
+# Done
 Write-Host ""
 Write-Host "Backup '$($BakupFolder)' restored!"
 Write-Host ""
